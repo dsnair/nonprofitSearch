6c023d778d601ef3318ac57c17a9d6335f794b37
Divya Nair <divnairs@gmail.com>
2018-09-17 18:06:09 -0700

    render marker cluster without reading cached file

--- src/App.js
+++ src/App.js
@@ -1,27 +1,50 @@
 import React from "react";
 import isEmpty from "lodash/isEmpty";
 
+import "./App.css";
 import MyMap from "./MyMap";
 import Form from "./Form";
-import "./App.css";
 
 const CITY = "Oakland";
 const STATE = "CA";
-const PAGE_SIZE = 5;
+const PAGE_SIZE = 10;
 
 const CN_API_KEY = "d79dd1ad7b20120353f5874ed6cd13d2";
 const APP_ID = "38f9f556";
-const URL = `https://api.data.charitynavigator.org/v2/Organizations?app_id=${APP_ID}&app_key=${CN_API_KEY}&state=${STATE}&city=${CITY}&pageSize=${PAGE_SIZE}`;
+const CN_URL = `https://api.data.charitynavigator.org/v2/Organizations?app_id=${APP_ID}&app_key=${CN_API_KEY}&state=${STATE}&city=${CITY}&pageSize=${PAGE_SIZE}`;
 
 class App extends React.Component {
   state = {
     response: {},
+    coords: [
+      {
+        ein: "010570536",
+        lat: 37.8043637,
+        lng: -122.2711137
+      },
+      {
+        ein: "010579792",
+        lat: 37.8343129,
+        lng: -122.2362901
+      }
+    ],
     error: false
   };
+
+  // componentDidMount() {
+  //   const readFile = async file => {
+  //     const request = await fetch(file);
+  //     const fileContents = await request.text();
+  //     this.setState({ coords: fileContents });
+  //     console.log("STATE", this.state.coords);
+  //   };
+  //   readFile("geocode.txt");
+  // }
+
   handleClick = async event => {
     event.preventDefault();
     try {
-      const request = await fetch(URL);
+      const request = await fetch(CN_URL);
       const response = await request.json();
       this.setState({
         response,
@@ -31,13 +54,12 @@ class App extends React.Component {
       this.setState({ error: true });
     }
   };
+
   render() {
     return (
       <React.Fragment>
         <Form onClick={this.handleClick} />
-        {!isEmpty(this.state.response) && (
-          <MyMap isMarkerShown className="Map" />
-        )}
+        {!isEmpty(this.state.response) && <MyMap coords={this.state.coords} />}
       </React.Fragment>
     );
   }
--- src/MyMap.js
+++ src/MyMap.js
@@ -1,28 +1,40 @@
 import React from "react";
-import { compose, withProps } from "recompose";
+import { compose, withProps, withHandlers } from "recompose";
 import {
   withScriptjs,
   withGoogleMap,
   GoogleMap,
   Marker
 } from "react-google-maps";
+import { MarkerClusterer } from "react-google-maps/lib/components/addons/MarkerClusterer";
 
 const G_API_KEY = "AIzaSyDIHtFTYSqg9qoqi-Bn8fLUjIH02sC5ddU";
 
 const MyMap = compose(
   withProps({
-    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${G_API_KEY}&v=3.exp&libraries=geometry,drawing,places`,
+    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${G_API_KEY}&v=3.exp`,
     loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px`, width: `400px` }} />,
+    containerElement: <div style={{ height: `800px`, width: `800px` }} />,
     mapElement: <div style={{ height: `100%` }} />
+  }),
+  withHandlers({
+    onMarkerClustererClick: () => markerClusterer =>
+      markerClusterer.getMarkers()
   }),
   withScriptjs,
   withGoogleMap
 )(props => (
-  <GoogleMap defaultZoom={14} defaultCenter={{ lat: 37.8044, lng: -122.2711 }}>
-    {props.isMarkerShown && (
-      <Marker position={{ lat: 37.8044, lng: -122.2711 }} />
-    )}
+  <GoogleMap defaultZoom={13} defaultCenter={{ lat: 37.8044, lng: -122.2711 }}>
+    <MarkerClusterer
+      onClick={props.onMarkerClustererClick}
+      averageCenter
+      enableRetinaIcons
+      gridSize={60}
+    >
+      {props.coords.map(coord => (
+        <Marker key={coord.ein} position={{ lat: coord.lat, lng: coord.lng }} />
+      ))}
+    </MarkerClusterer>
   </GoogleMap>
 ));
 
